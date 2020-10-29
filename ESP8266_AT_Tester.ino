c9878a5a37eec9a436ea66dbe7c37ecf8cf940ae
M.Henry <39671572+mhenry20@users.noreply.github.com>
2020-10-29 19:19:18 -0400

    Apply Standards

--- ESP8266_AT_Tester.ino
+++ ESP8266_AT_Tester.ino
@@ -1,30 +1,14 @@
 /*
- * MHENRY: 10/28/2020
+ * Author: Mike Henry 10/28/2020
  * Program for testing AT commands with a Arduino connected to a ESP WiFI
  * board.
  * Use Serial Monitor to send AT commands and see results.
  */
 
-
 int i=0;
 char rx_string[500];
 
-
-boolean DEBUG=true;
-
-void showResponse(int waitTime){
-    long t=millis();
-    char c;
-    while (t+waitTime>millis()){
-      if (Serial3.available()){
-        c=Serial3.read();
-        if (DEBUG) Serial.print(c);
-      }
-    }                   
-}
-
 void setup() {
-  DEBUG=true;
 
   rx_string[0]=0;
 
@@ -34,11 +18,11 @@ void setup() {
   Serial.println("Serial Talk back to Arduino.");
   
   Serial3.println("AT+RST");          // if needed, Enable this AT command line to reset the module;
-  showResponse(2000);
+  display_serial(2000);
 
   Serial3.println("AT+GMR");
   Serial.println("Response from AT+GMR");
-  showResponse(2000);
+  display_serial(2000);
   
 }
 
@@ -47,7 +31,6 @@ char rx_byte = 0;
 
 if (Serial.available() > 0) {    // is a character available?
     rx_byte = Serial.read();       // get the character
-    
     rx_string[i] = rx_byte;
     i++;
     if( rx_byte == '\n' ){
@@ -56,9 +39,20 @@ if (Serial.available() > 0) {    // is a character available?
        Serial.println(rx_string);
        Serial3.println(rx_string);
        Serial.println("Received: ");
-       showResponse(2000);
+       display_serial(2000);
        i=0;
        rx_string[i] = 0;
     }
    } 
+}
+
+void display_serial(int wait_time){
+    long t=millis();
+    char c;
+    while (t+wait_time>millis()){
+      if (Serial3.available()){
+        c=Serial3.read();
+        Serial.print(c);
+      }
+    }                   
 }
